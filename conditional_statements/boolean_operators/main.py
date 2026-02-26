4d8865d55c215cc69d3779e6af65bd2c8e407261
Hackth3br0nx <>
2026-02-26 01:24:15 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,11 @@
+# Define cost
+total_cost = 25.00
+
+# define threshould
+
+threshold = 20.00
+
+# define discount
+discountEligible = total_cost >= threshold
+
+print("Is the purchase eligible for a discount?", discountEligible)
