c4bfef3e1b16ff7c4037dd85a720fe4a73a3ffe0
Hackth3br0nx <>
2026-03-04 02:12:08 +0000

    Challenge: List Management

--- other_data_types/challenge_list_management/main.py
+++ other_data_types/challenge_list_management/main.py
@@ -1,12 +1,25 @@
 # Define lists
-meat = ["Ham, 3.99, 50 "Sliced"]
+meat = ["Ham", 3.99, 50, "Sliced"]
 cheese = ["Cheddar", 5.49, 100, "Sharp"]
-condiment = "Mustaaaaaaaaaaaaard", 1.99, 75, "Spicy"
+condiment = ["Mustaaaaaaaaaaaaard", 1.99, 75, "Spicy"]
 
 # Define Main list
-deli_dept = [meat + cheese + condiment]
+deli_dept = [meat, cheese, condiment]
+print(f"Initial Deli List: {deli_dept}")
+
 
 # Restock items
-if meat["Ham"] = < 100:
-    meat[0] = 100
-    
+if "Ham" in meat and meat[2] < 100:
+    meat[2] = 100
+
+# add seasonal meat
+seasonal_meat = ["Turkey", 4.50, 100, "Sliced"]
+deli_dept.append(seasonal_meat)
+
+
+
+# remove condiment
+deli_dept.remove(condiment)
+deli_dept.sort()
+
+print(f"Updated Deli List: {deli_dept}")
