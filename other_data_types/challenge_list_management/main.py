1bf0eba6d9477f42ed77f8135b618dc6558fec56
Hackth3br0nx <>
2026-03-04 01:33:52 +0000

    Challenge: List Management

---
+++ other_data_types/challenge_list_management/main.py
@@ -0,0 +1,12 @@
+# Define lists
+meat = ["Ham, 3.99, 50 "Sliced"]
+cheese = ["Cheddar", 5.49, 100, "Sharp"]
+condiment = "Mustaaaaaaaaaaaaard", 1.99, 75, "Spicy"
+
+# Define Main list
+deli_dept = [meat + cheese + condiment]
+
+# Restock items
+if meat["Ham"] = < 100:
+    meat[0] = 100
+    
